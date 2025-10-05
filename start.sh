36ef597247c7863d3258cbfb1dd10268cf0f54f2
AngelGab007 <angelbeltrea.20@gmail.com>
2025-10-05 02:44:25 -0400

    render changes

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-# Usa Gunicorn para arrancar tu app.py
-# El formato es: gunicorn [nombre_del_archivo_flask]:[nombre_de_la_instancia_flask]
-gunicorn main:app
+gunicorn app:app 
